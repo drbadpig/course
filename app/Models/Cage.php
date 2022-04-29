960a8e922a240a0629a18240e30e0b70fb59f18c
DrBadPig <stuartkirk14@gmail.com>
2022-04-29 23:30:53 +0300

    improve Cage and Department models

--- app/Models/Cage.php
+++ app/Models/Cage.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Cage extends Model
 {
     use HasFactory;
+
+    /**
+     * Get the Department associated with the cage.
+     */
+    public function department()
+    {
+        return $this->belongsTo(Department::class);
+    }
 }
--- app/Models/Department.php
+++ app/Models/Department.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Department extends Model
 {
     use HasFactory;
+
+    /**
+     * Get the Cages associated with the Department.
+     */
+    public function cages()
+    {
+        return $this->hasMany(Cage::class);
+    }
 }
