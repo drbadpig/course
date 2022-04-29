338f5ef43818eb2e4da54959ceb5ea42f2dd9cac
DrBadPig <stuartkirk14@gmail.com>
2022-04-29 22:32:26 +0300

    add workers table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\Worker;
 
 return new class extends Migration
 {
@@ -16,11 +17,12 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->unsignedBigInteger('worker_id')->unique()->nullable();
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
---
+++ database/migrations/2022_04_29_143512_create_workers_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('workers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->unsignedInteger('age');
+            $table->unsignedInteger('gender');
+            $table->string('passport')->unique();
+            $table->string('address');
+            $table->date('date_of_birth');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        Schema::table('users', function (Blueprint $table) {
+            $table->foreign('worker_id')->references('id')->on('workers');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign('users_worker_id_foreign');
+        });
+
+        Schema::dropIfExists('workers');
+    }
+};
