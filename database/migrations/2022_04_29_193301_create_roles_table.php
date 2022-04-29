2218787610dd9bad52d9a3aa92a1cc6103d0168b
DrBadPig <stuartkirk14@gmail.com>
2022-04-29 22:42:59 +0300

    add roles table

---
+++ database/migrations/2022_04_29_193301_create_roles_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        Schema::table('workers', function (Blueprint $table) {
+            $table->unsignedBigInteger('role_id')->after('gender');
+            $table->foreign('role_id')->references('id')->on('roles');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('workers', function (Blueprint $table) {
+            $table->dropForeign('workers_role_id_foreign');
+            $table->dropColumn('role_id');
+        });
+
+        Schema::dropIfExists('roles');
+    }
+};
