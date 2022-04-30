5c56b9d940cb02ec3fb336e0a09c1aec0c246806
DrBadPig <stuartkirk14@gmail.com>
2022-04-30 16:38:54 +0300

    add chickens table

---
+++ database/migrations/2022_04_30_132627_create_chickens_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('chickens', function (Blueprint $table) {
+            $table->id();
+            $table->date('date_of_birth');
+            $table->double('weight');
+            $table->boolean('state');
+            $table->unsignedBigInteger('breed_id');
+            $table->unsignedBigInteger('cage_id');
+            $table->unsignedBigInteger('worker_id');
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('breed_id')->references('id')->on('breeds');
+            $table->foreign('cage_id')->references('id')->on('cages');
+            $table->foreign('worker_id')->references('id')->on('workers');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('chickens');
+    }
+};
